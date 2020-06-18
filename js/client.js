a6f09eae4255d169d9a406e5b0609807d60c735a
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-06-19 01:44:55 +0530

    fixed server issues

--- js/client.js
+++ js/client.js
@@ -1,7 +1,11 @@
+var socket = io('http://localhost:8000');
 const form = document.getElementById('sendcontainer');
 const messageInput = document.getElementById('messageinp');
 const messageContainer = document.querySelector(".container");
-var socket = io('http://localhost:8000');
+
+const name = prompt("Enter your name to join");
+socket.emit('new_user_joined', name);
+
 const append = (message, position) => {
     const messageelement = document.createElement('div');
     messageelement.innerText = message;
@@ -12,16 +16,13 @@ const append = (message, position) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
-    append('you:${message}', 'right');
+    append('You :' + message, 'left');
     socket.emit('send', message);
     messageInput.value = '';
-})
-const name = prompt("Enter your name to join");
-socket.emit('new_user_joined', name);
-
+});
 socket.on('user_joined', name => {
-    append('${name} joined the chat', 'right');
-})
+    append(`${name} joined the chat`, 'right');
+});
 socket.on('receive', data => {
-    append('${data.message}:${data.user}', 'left');
-})
+    append(`${data.name}:${data.message}`, 'right');
+});
--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,12 +1,12 @@
-const io = require('socket.io')(8000)
-const users = {};
+const io = require('socket.io')(8000);
+var users = {};
 io.on('connection', socket => {
     socket.on('new_user_joined', name => {
-        console.log("new_user", name)
+        console.log("new_user", name);
         users[socket.id] = name;
         socket.broadcast.emit('user_joined', name);
     });
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
-    })
+    });
 });
